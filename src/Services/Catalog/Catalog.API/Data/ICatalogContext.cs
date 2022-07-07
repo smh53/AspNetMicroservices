a27b74693d6b5ef8ae95c13a425b93a26ba62b12
Semih Tavukçu <semih.tavukcu@outlook.com>
2022-07-07 23:58:18 +0300

    added some stuff

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; } 
+
+    }
+}
